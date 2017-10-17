0d59ce2ae7aec41eec81c8e13a7af3b49c050a6b
Luwi Lagura <jllagura@users.noreply.github.com>
2017-10-17 15:05:04 +0800

    updated with men and women's hairstyles
    - layouts of every tutorial (women's hairstyle)
    - men's hairstyle (not clickable not yet tested)

--- app/src/main/java/com/example/tambalo/thesisynet/MainActivity.java
+++ app/src/main/java/com/example/tambalo/thesisynet/MainActivity.java
@@ -12,7 +12,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void topKnotOnClick(View view){
-        
+    public void finHairstyleOnClick(View view){
+        setContentView(R.layout.finhairstyle);
     }
+    public void celticKnotOnClick(View view){
+        setContentView(R.layout.celticknot);
+    }
+    public void halfupHairstyleOnClick(View view){
+        setContentView(R.layout.halfuphairstyle);
+    }
+    public void halfupFrenchbraidOnClick(View view){
+        setContentView(R.layout.halfupfrenchbraids);
+    }
+    public void frenchHairstyleOnClick(View view){
+        setContentView(R.layout.frenchhairstyle);
+    }
+    public void topBunOnClick(View view){
+        setContentView(R.layout.topbun);
+    }
+    public void promOnClick(View view){
+        setContentView(R.layout.promhairstyle);
+    }
+    public void ladderHairstyleOnClick(View view){
+        setContentView(R.layout.ladderhairstyle);
+    }
+    public void textureLooseOnClick(View view){
+        setContentView(R.layout.texturelooseupdo);
+    }
+    public void dutchHairstyleOnClick(View view){
+        setContentView(R.layout.dutchhairstyle);
+    }
+    public void braidedBunOnClick(View view){
+        setContentView(R.layout.braidedbun);
+    }
+    public void sidesweptWatBraidOnClick(View view){
+        setContentView(R.layout.sidesweptwatbraid);
+    }
+    public void topHalfOnClick(View view){
+        setContentView(R.layout.tophalf);
+    }
+    public void cinnamonOnClick(View view){
+        setContentView(R.layout.cinnamon);
+    }
+
 }
